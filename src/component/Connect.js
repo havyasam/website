ff6df0dda425beb42f6e813b2b3b03cd21fd5aec
havyas <havyasagaltha@gmail.com>
2023-11-20 13:17:11 +0530

    made some changes in contact section

--- src/component/Connect.js
+++ src/component/Connect.js
@@ -1,32 +1,50 @@
+import React, { useRef } from 'react';
+import emailjs from '@emailjs/browser';
 import '../css/Connect.css'
+
+ 
 function Connect(){
+  const form = useRef();
+
+ const  sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm('service_a2yo84p', 'template_ks47d5r', form.current, 'NaVcb9oyAxxSXGBMY')
+      .then((result) => {
+          alert("message sent")
+          form.current.reset()
+      }, (error) => {
+          console.log(error.text);
+      });
+  };
     return(
         <>
         <div class="contact-section">
-    <h2 class="contact-heading">Contact Me</h2>
-    <div class="contact-form">
-      <form action="#" method="post">
-        <div class="form-group">
-          <label for="name">Your Name:</label>
-          <input type="text" className="inpt" id="name" name="name" required/>
-        </div>
+        <h2 class="contact-heading">Contact Me</h2>
+        <div class="contact-form">
+        <form ref={form} onSubmit={sendEmail}>
+            <div class="form-group">
+              <label for="name">Your Name:</label>
+              <input type="text" className="inpt" id="name" name="user-name" required/>
+            </div>
 
-        <div class="form-group">
-          <label for="email">Your Email:</label>
-          <input type="email" id="email" name="email" required/>
-        </div>
+            <div class="form-group">
+              <label for="email">Your Email:</label>
+              <input type="email" id="email" name="email" required/>
+            </div>
 
-        <div class="form-group">
-          <label for="message">Your Message:</label>
-          <textarea id="message" name="message" rows="4" required></textarea>
-        </div>
+            <div class="form-group">
+              <label for="message">Your Message:</label>
+              <textarea id="message" name="message" rows="4" required></textarea>
+            </div>
 
-        <div class="form-group">
-          <button type="submit">Send Message</button>
+            <div class="form-group">
+              <button type="submit" >Send Message</button>
+            </div>
+          </form>
         </div>
-      </form>
-    </div>
-  </div>
+      </div>
+      
         </>
     )
 
